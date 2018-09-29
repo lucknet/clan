3199869b72a28248d76faf2ef98e3fbce29118f3
yude <yudesleepy@gmail.com>
2018-09-29 15:20:38 +0900

    First commit

---
+++ assets/index_.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>#Luck Official Website | ホーム / Home</title>
+   <meta name="description" content="Minecraftや、その他FPSゲームなど幅広い分野で活動しているクラン「#Luck」の公式ウェブサイトです。" />
+  <meta content="Luck Network | ホーム" name="title">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="Keywords" content="Minecraft,Luck,Anni,LuckNetwork,Luck Network,マインクラフト">
+  <meta name="copyright" content="&copy 2017-<?php echo date('Y'); ?> Luck Network">
+  <meta name="author" content="Luck Network">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:domain" content="lucknetwork.jp" />
+  <meta name="twitter:site" content="@McLuckServer">
+  <meta property="og:site_name" content="#Luck Official Website">
+  <meta property="og:title" content="#Luck Official Website">
+  <meta property="og:description" content="Minecraftや、その他FPSゲームなど幅広い分野で活動しているクラン「#Luck」の公式ウェブサイトです。">
+  <link rel="shortcut icon" type="image/jpg" href="assets/img/clover.jpg" />
+  <meta property="og:type" content="website">
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet" />
+  <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/minty/bootstrap.min.css" rel="stylesheet" integrity="sha384-4eGtnTOp6je5m6l1Zcp2WUGR9Y7kJZuAiD3Pk2GAW3uNRgHQSIqcrcAxBipzlbWP" crossorigin="anonymous">
+  <style type="text/css" media="screen">
+  	body,*{font-family: "Rounded Mplus 1c";}
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">#Luck</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">ホーム <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="term">規約</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
